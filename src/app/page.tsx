f540327e925a2e5c0995351beb54ea01c21e5c21
soldev22 <mike@solutionsdeveloped.co.uk>
2026-04-23 08:33:15 +0100

    V1-Version 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-full flex-1 items-center justify-center">
-      <h1 className="text-3xl font-semibold">Hello world - Mike Catignani</h1>
+      <h1 className="text-3xl font-semibold">Hello world - Mike Catignani - Version 2</h1>
     </main>
   );
 }
